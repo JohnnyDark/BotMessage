e70765e0ad4590ffd9e61c19088b8e6722718e08
Johnny.Wong <13540294001@163.com>
2018-07-09 10:22:11 +0800

    modify new cofig comment information

--- chat_room.py
+++ chat_room.py
@@ -4,6 +4,8 @@ from messagebot.all_methods import chat_room, send_message_in_room
 
 
 def invite_to_chatroom():
+
+    """创建聊天室并在聊天室中发送消息"""
     room_name = input('please input the room name: ')
     room_id = chat_room(room_name)
     send_message_in_room(room_id)
@@ -11,4 +13,4 @@ def invite_to_chatroom():
 
 if __name__ == '__main__':
     invite_to_chatroom()
-   
+
